19f9a1c73fccde0d243f02a929edd51a550df125
PiquiJL01 <41171903+PiquiJL01@users.noreply.github.com>
2021-07-02 13:02:30 -0400

    Conversion
    Se agrega una clase de conversion, se implementa las conversiones

--- bases-uno/Engine/Classes/Constants.cs
+++ bases-uno/Engine/Classes/Constants.cs
@@ -13,4 +13,21 @@ namespace Engine.Classes
         public const string State = "Estado";
         public const string Country = "Pais";
     }
+
+    public static class Conversion
+    {
+        #region Conversion de Monedas
+        private const double Euro = 1.12;
+
+        public static double ConvertirDolares(double dolares)
+        {
+            return dolares / Euro;
+        }
+
+        public static double ConvertirEuros(double euros)
+        {
+            return Euro * euros;
+        }
+        #endregion
+    }
 }
