aaffb841014e15ccf482926a7225ba351fae99e9
abadillo <abadillo2924@gmail.com>
2021-07-14 10:06:51 -0400

    Add inserts_viejo.sql

---
+++ docs/DDL Bases de Datos/inserts_viejo.sql
@@ -0,0 +1,27 @@
+INSERT INTO public.lugar (nombre,tipo,lugar_id) VALUES
+	 ('Venezuela','Pais',NULL),
+	 ('Av Santa Rosa, con 3era trasversal','Direccion',3),
+	 ('Distrito Capital','Estado',1),
+	 ('Caracas','Ciudad',2),
+	 ('La Viña','Estado',3),
+	 ('Los teques','Ciudad',3),
+	 ('Mexico','Pais',NULL);
+
+INSERT INTO public.comic (titulo,volumen,numero,fecha_publicacion,precio_publicacion,color,sinopsis,paginas,cubierta,editor) VALUES
+	 ('Dark Souls II: Into the Light',1,2,'2017-05-15',30.0,true,'Deep within the Shaded Woods, a lone warrior donning the Faraam Set encounters a man sitting next to a bonfire. The warrior tells the man that he doesn''t want to cause any harm and advises him to extinguish the bonfire as quickly as possible since it could attract other people with murderous intent. The man appreciates the kind words of the warrior and invites him to sit around the fire for a while. He introduces himself as Saulden and then proceeds to ask the warrior why he has come to "these foul lands" to which the warrior only answers that he has come to find a cure for a curse and promptly moves on. Saulden advises against it, telling the curse bearer that "A blade cannot cut the darkness. Only the flame of a fire can do so." The warrior cockily ignores his advice and remarks that Saulden''s sword "belongs to a crestfallen man who hides by his fire", before continuing on his journey',10,false,'Rob Williams'),
+	 ('Contra: The Comic',1,1,'1998-02-05',10.0,false,'With the last remnants of the alien menace nearly eradicated thanks to the hard work of the Earth Government Forces and wonderful citizens like you, the situation is completely under control,',24,true,'unknown'),
+	 ('Cyberpunk 2077: Trauma Team',1,1,'2021-12-02',50.0,true,'Set in the year 2077, the plot follows Nadia, an EMT for the privately owned and heavily militarized healthcare company named Trauma Team International. When a shootout leaves the rest of her team dead, Nadia agrees to continue working for the company',12,true,'Cullen Bunn '),
+	 ('Titul',NULL,0,'2001-11-11',NULL,false,'algo aqui ',0,false,'edito'),
+	 ('Tekken Comic',2,5,'2005-02-02',10.0,true,'Jin Kazama, the owner of the powerful Japanese corporation Mishima Zaibatsu, declares war on the entire world. Nations that sided with the corporation merged into one independent nation, destroying any opposition. A year later, the Mishima Zaibatsu organization opens "The King of Iron Fist Tournament", declaring that the victor will be guaranteed a great prize. However, the tournament is interrupted by the appearance of Asuka Kazama who attacks Jin and demanded that he must stop his despotic ambitions carried out through Mishima Zaibatsu. Jin then orders guards—led by Eddy Gordo—to remove her from the premises. Little does Asuka know that her actions eventually lead to very unexpected consequences later on..',30,true,'Ruy Takato'),
+	 ('The Last of Us American Dreams',4,4,'2015-02-11',15.0,true,'At the Boston quarantine zone, Ellie becomes caught in a fight with a group of boys who attempt to steal her belongings. Riley Abel steps in to break up the fight, beating one of the boys and making the others flee. Ellie becomes angered by this, claiming that she can take care of herself. Riley then advises Ellie to flee, but the latter is caught and assigned to cleaning duty. While she is completing the chore, she realizes that Riley stole her Walkman, and later demands that she return it, which Riley reluctantly does',20,true,'Neil Druckmann'),
+	 ('The Halo Graphic Novel',1,3,'2005-11-15',30.0,true,'"The Last Voyage of the Infinite Succor" takes place during the video game Halo: Combat Evolved. The Covenant Special Operations Commander Rtas ''Vadumee and his team are sent to answer the distress call from a Covenant agricultural ship, Infinite Succor. Believing that it might have been attacked by humans, ''Vadumee and his team instead discover the ship has been infested by the parasitic Flood, who gain the knowledge of those they infect and are trying to use the ship to escape imprisonment. Fighting waves of Flood, including the reanimated remains of his fallen soldiers, ''Vadumee plots a slipspace course that will destroy both Infinite Succor and the Flood, then escapes via a Covenant shuttle. ',40,true,'Peter David'),
+	 ('3432432',NULL,0,'1231-12-12',NULL,true,'3123121',0,false,'21321312'),
+	 ('sfdskfm',2,2,'1232-12-12',2.0,true,'fdsfsdfd',2,false,'safsdsdfsaddsfdssdaf'),
+	 ('dsf',NULL,0,'2001-11-11',23123213317245500000,false,'sd',0,false,'sdds');
+
+INSERT INTO public.interes (nombre,descripcion) VALUES
+	 ('Consola Nintendo 64','Controles, juegos, accessorios y ediciones limitadas de la consola Nintendo 64'),
+	 ('Juegos de Mesa','Ajedrez, damas chinas, domino, etc'),
+	 ('Tetris NES','Juego clasico desde 1984'),
+	 ('Poker','El juego de cartas mas popular del mundo'),
+	 ('Juego PS2 y PS1','Lo mejor en juegos de la 5ta y 6ta generacion de consolas de Sony');
