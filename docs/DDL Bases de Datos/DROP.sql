bf3b3e88eec230aa62e2cd7db9dd040141b1fa8d
abadillo <abadillo2924@gmail.com>
2021-07-14 00:20:28 -0400

    Add CREATE.sql

---
+++ docs/DDL Bases de Datos/DROP.sql
@@ -0,0 +1,41 @@
+DROP TABLE public.clu_int;
+
+DROP TABLE public.contacto;
+
+DROP TABLE public.interes;
+
+DROP TABLE public.listado;
+
+DROP TABLE public.org_inv;
+
+DROP TABLE public.org_sub;
+
+DROP TABLE public.organizacion_caridad;
+
+DROP TABLE public.participante;
+
+DROP TABLE public.subasta;
+
+DROP TABLE public.dueno_historico;
+
+DROP TABLE public.coleccionable;
+
+DROP TABLE public.comic;
+
+DROP TABLE public.membresia;
+
+DROP TABLE public."local";
+
+DROP TABLE public.club;
+
+DROP TABLE public.coleccionista;
+
+DROP TABLE public.representante;
+
+DROP TABLE public.lugar;
+
+
+
+
+
+
