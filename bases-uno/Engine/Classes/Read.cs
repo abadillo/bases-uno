279d48e8846504b3edcf6a33be35b85bd81e1e0a
PiquiJL01 <41171903+PiquiJL01@users.noreply.github.com>
2021-07-15 12:40:36 -0400

    Update Club

--- bases-uno/Engine/Classes/Read.cs
+++ bases-uno/Engine/Classes/Read.cs
@@ -61,7 +61,7 @@ namespace Engine.Classes
 
                 if (connection.Reader.Read())
                 {
-                    club = new Club(connection.ReadInt(0), connection.ReadDate(1), connection.ReadString(4), 
+                    club = new Club(connection.ReadInt(0), connection.ReadString(7), connection.ReadDate(1), connection.ReadString(4), 
                         connection.ReadInt(5), connection.ReadInt(2), connection.ReadString(3));
                 }
             }
@@ -607,7 +607,7 @@ namespace Engine.Classes
 
                 while (connection.Reader.Read())
                 {
-                    Club club = new Club(connection.ReadInt(0), connection.ReadDate(1), connection.ReadString(4),
+                    Club club = new Club(connection.ReadInt(0), connection.ReadString(7), connection.ReadDate(1), connection.ReadString(4),
                         connection.ReadInt(5), connection.ReadInt(2), connection.ReadString(3));
 
                     list.Add(club);
